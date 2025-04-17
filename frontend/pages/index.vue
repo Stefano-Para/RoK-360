53716340039cbaa3afeaa60e1551f9750921ef38
Stefano-Para <stefano.zina@gmail.com>
2025-04-17 12:44:46 +0200

    try to redeply

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -9,7 +9,7 @@ useHead({
 
 <template>
     <section class="flex flex-col items-center justify-center text-center p-6">
-      <h1 class="text-5xl font-bold mb-4">Benvenuto su RoK-360</h1>
+      <h1 class="text-5xl font-bold mb-4">Benvenuto su RoK-360!</h1>
       <p class="text-xl mb-6">Il tuo sito per esplorare Rise of Kingdoms come mai prima</p>
       <NuxtLink to="/aboutUs" class="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition">
         Scopri chi siamo
